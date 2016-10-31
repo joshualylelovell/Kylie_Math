bf35367d9cdaf33436e08a17ebc51686eddae6e7
Joshua Lovell <joshualylelovell@gmail.com>
2016-10-31 13:57:36 -0400

    on submit now returns false so that page is not refreshed

--- js/app.js
+++ js/app.js
@@ -6,6 +6,16 @@ var y = [1,2,3,4,5,6,7,8,9,10]
 var gameX = x[Math.floor(Math.random() * x.length)];
 var gameY = y[Math.floor(Math.random() * y.length)];
 
+var numberCorrect = 0;
+
+var correctPhrases = ["Awesome!", "Amazing!", "Math Whiz!", "Excellent!"]
+
+function pushCorrectPhrases() {
+  var setCorrectPhrases = correctPhrases[Math.floor(Math.random() * correctPhrases.length)];
+  document.getElementById("correctPhrasesDiv").innerHTML = setCorrectPhrases;
+  console.log(setCorrectPhrases);
+}
+
 function setGameBoard() {
   if (gameX > gameY) {
     document.getElementById("x").innerHTML = gameX.toString()
@@ -43,37 +53,37 @@ function setSubtract() {
 /***************
 GAME PLAY
 ***************/
+// $(document).ready(function() {
+console.log( "ready!" );
 
-$(document).ready(function() {
-    console.log( "ready!" );
+      $("#play-addition").on( "click", function() {
+        setAdd();
+        setGameBoard();
+        answerAdd();
+        console.log(answerAdd(gameX, gameY));
+      });
 
-    $("#play-addition").on( "click", function() {
-      setAdd();
-      setGameBoard();
-      answerAdd();
-      console.log(answerAdd(gameX, gameY));
-    });
+      $("#play-subtraction").on( "click", function() {
+        setSubtract();
+        setGameBoard();
+        answerSubtract();
+        console.log(answerSubtract(gameX, gameY));
+      });
 
-    $("#play-subtraction").on( "click", function() {
-      setSubtract();
-      setGameBoard();
-      answerSubtract();
-      console.log(answerSubtract(gameX, gameY));
-    });
+      $("#answerForm").submit(function(){
+        var userAnswer = $("#userAnswer").val()
+        function userAnswerToInt (userAnswer) {
+          return parseInt(userAnswer, 10);
+        }
+        console.log(userAnswer);
+      	if ( userAnswerToInt(userAnswer) === answerAdd(gameX, gameY)) {
+          pushCorrectPhrases();
+        } else if ( userAnswerToInt(userAnswer) === answerSubtract(gameX, gameY)) {
+          pushCorrectPhrases();
+        } else {
+          document.getElementById("correctPhrasesDiv").innerHTML = "Try again. You can do it.";
+        }
+        return false;
+      });
 
-    $("#answerForm").submit(function(){
-      var userAnswer = $("#userAnswer").val()
-      function userAnswerToInt (userAnswer) {
-        return parseInt(userAnswer, 10);
-      }
-      console.log(userAnswer);
-    	if ( userAnswerToInt(userAnswer) === answerAdd(gameX, gameY)) {
-        alert("CORRECT")
-      } else if ( userAnswerToInt(userAnswer) === answerSubtract(gameX, gameY)) {
-        alert("CORRECT")
-      } else {
-        alert("TRY AGAIN")
-      }
-    });
-
-});
+// });
